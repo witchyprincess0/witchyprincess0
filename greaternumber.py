79156d4c8346fcad65673e81741dbb7743fbb8c4
witchyprincess0 <99764946+witchyprincess0@users.noreply.github.com>
2022-02-16 13:10:32 -0800

    Create greaternumber.py

---
+++ greaternumber.py
@@ -0,0 +1,8 @@
+
+N1 = input('whtat is the first number?')
+N2  = input ('what is the second second?')
+
+if int(N1) > int(N2):
+  print (f'{N1} is greater than {N2}')
+else:
+   print((f'{N2} is greather than {N1}'))
