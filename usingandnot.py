d5686a79bd6b64487a23e0ec2e13828817a5b693
witchyprincess0 <99764946+witchyprincess0@users.noreply.github.com>
2022-02-15 20:37:03 -0800

    Create usingandnot.py

---
+++ usingandnot.py
@@ -0,0 +1,9 @@
+is_magician = False
+is_expert = True
+
+if is_magician and is_expert:
+  print('you are a master magician.')
+elif is_magician and not is_expert:
+  print ('you are getting there.')
+else:
+  print('you need magic powers')
